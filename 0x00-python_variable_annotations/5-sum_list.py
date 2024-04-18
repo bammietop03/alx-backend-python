ab799fcdc2db2b18757d6c48429bbbb4d792e2ce
Bamiwo Adebayo <bammietop@gmail.com>
2024-04-18 17:27:29 +0100

    Python Variable Annotation

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function sum_list which takes a list input_list
+ of floats as argument and returns their sum as a float.
+"""
+import typing
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    # sum: float = 0
+    # for x in input_list:
+    #     sum += x
+
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+Learning Python Variable Annotations
