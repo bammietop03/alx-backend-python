e58955ef91ac604ff759159f6a1acc67641bf5c0
Bamiwo Adebayo <bammietop@gmail.com>
2024-04-18 21:21:13 +0100

    Python Variable Annotation

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,7 +6,7 @@ and apply any necessary changes.
 from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List[int]:
     """ Using mypy """
     zoomed_in: List[int] = [
         item for item in lst
